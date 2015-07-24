862a56d7a0b199fb48c24c0009bd818bb682a25b
李俊良 <lijunliang6@wanda.cn>
2015-07-25 00:02:28 +0800

    自动创建订单

--- controllers/order/master.js
+++ controllers/order/master.js
@@ -10,10 +10,51 @@ module.exports = {
                 }
                 if(!ret || ret.length <= 0){
                     // 如果不存在就创建订单
-                    return res.json({
-                        status: 500,
-                        msg: '请先创建订单'
-                    })
+                    // var orderdate ="20121313";
+                    var date = new Date();
+                    var orderdate = date.getFullYear()+""
+                            + (date.getMonth()+1 < 10 ? ("0"+(date.getMonth()+1).toString()):(date.getMonth()+1).toString())
+                            + (date.getDate() < 10 ? ("0"+(date.getDate()).toString()):(date.getDate()).toString());
+                    var autoInsert = "insert into ordermaster(order_oper, supplie_id, order_date, order_type, comp_id, order_status) values('"+req.session.username+"',"+supplie_id+","+orderdate+", 1,"+req.session.comp_id+",1)";
+                    conn.query(autoInsert, function(err, autoResult){
+                        if(err){
+                            return res.json({status: 500, err: err.message});
+                        }
+                        conn.query(sql, function(err, ret2){
+                            if(err){
+                                return res.json({status: 500, err: err.message});
+                            }
+                            if(!ret2 || ret2.length <= 0 ){
+                                return ret2.json({status: 500, err: '自动新建订单失败'});
+                            }
+                            var order_id = ret2[0]['order_id'];
+                            var sqlInsert = "insert into orderdetail(order_id, good_id, good_number,good_price, good_amount, good_name,good_gg,good_dw, good_cp, good_pzwh) values";
+                            var insert_array = [
+                                    "('"+order_id+"'",
+                                    parseInt(req.body.good_id),
+                                    parseInt(req.body.number),
+                                    "(select good_price from good_info where good_id="+parseInt(req.body.good_id)+")",
+                                    0,
+                                    "(select good_name from good_info where good_id="+parseInt(req.body.good_id)+")",
+                                    "(select good_gg from good_info where good_id="+parseInt(req.body.good_id)+")",
+                                    "(select good_dw from good_info where good_id="+parseInt(req.body.good_id)+")",
+                                    "(select good_cp from good_info where good_id="+parseInt(req.body.good_id)+")",
+                                    "(select good_pzwh from good_info where good_id="+parseInt(req.body.good_id)+"))"
+                            ];
+                            conn.query(sqlInsert+insert_array.join(','), function(err, ret){
+                                if(err){
+                                    return res.json({
+                                        status: 500,
+                                        msg: err.message
+                                    });
+                                }
+                                return res.json({
+                                    status: 200,
+                                    insertId: ret.insertId
+                                })
+                            });
+                        });
+                    });
                 }else{
                     // 如果存在直接加入到订单中
                     var order_id = ret[0].order_id;

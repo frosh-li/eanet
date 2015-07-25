ced1ab0a00b81f536b0958512fdac10e6276b6ee
李俊良 <lijunliang6@wanda.cn>
2015-07-25 21:21:51 +0800

    getVertion

--- auth/index.js
+++ auth/index.js
@@ -1,6 +1,6 @@
 module.exports = function (req, res, next) {
 	console.log(req.path);
-	if(!req.session.uid && req.path != "/api/user/login") {
+	if(!req.session.uid && req.path != "/api/user/login" && req.path != "/api/app/getVersion") {
 		return res.json({status: 302, msg:'please login'});
 	}else{
 		next()

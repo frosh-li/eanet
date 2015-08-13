bcc164c33d5592c02a3474eb48a05099f447d4fa
jl <s9c9jwef>
2015-08-13 22:54:06 +0800

    修改不出现订单的bug

--- controllers/order/rejectOrder.js
+++ controllers/order/rejectOrder.js
@@ -27,12 +27,14 @@ module.exports = {
             var type = req.query.type || "comp_id";
             var sql = 'select count(*) as total from ordermaster where order_reject != 0 and ';
             sql += type+'=';
-            var sql2 = 'select ordermaster.*,comp_info.name,count(orderdetail.oid) as itemcount ,SUM(orderdetail.good_amount) as total_amount from ordermaster,comp_info,orderdetail where comp_info.id=ordermaster.supplie_id and orderdetail.order_id=ordermaster.order_id and ordermaster.order_reject != 0 and ';
-            sql2 += type + '=';
+            var sql2 = 'select d.*, c.itemcount ,c.good_amount as total_amount from (select ordermaster.*,comp_info.name  from comp_info,ordermaster where order_reject != 0 and comp_info.id = ordermaster.supplie_id and ';
+
+            //'select ordermaster.*,comp_info.name,count(orderdetail.oid) as itemcount ,SUM(orderdetail.good_amount) as total_amount from comp_info, ordermaster left join orderdetail on orderdetail.order_id=ordermaster.order_id where comp_info.id=ordermaster.supplie_id and ordermaster.order_reject != 0 and ';
+            sql2 += "ordermaster."+type + '=';
             sql += req.session.comp_id;
             sql2 += req.session.comp_id;
-
-            sql2 += ' order by order_id desc';
+            sql2 +=') as d left join (select order_id,count(oid) as itemcount, sum(orderdetail.good_amount) as good_amount from orderdetail GROUP BY order_id) as c on d.order_id=c.order_id';
+            sql2 += ' order by d.order_id desc';
             console.log(sql);
             console.log(sql2);
             pool.getConnection(function(err, conn) {

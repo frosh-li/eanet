11a0081fa9c99af5673d139d483cd609f87ca5dd
李俊良 <lijunliang6@wanda.cn>
2015-09-16 17:57:35 +0800

    药品目录搜索处理

--- controllers/items/map.js
+++ controllers/items/map.js
@@ -37,13 +37,27 @@ module.exports = {
             var limit = parseInt(req.query.count) || 10;
             var page = parseInt(req.query.page) || 1;
             var start = (page-1)*limit;
+		    var good_name = req.query.good_name && decodeURIComponent(req.query.good_name);
+		    var good_cp = req.query.good_cp && decodeURIComponent(req.query.good_cp);
+		    var filter = [];
+		    if(good_name){
+				filter.push(" good_name like '%"+good_name+"%' ");
+		    }
+		    if(good_cp){
+				filter.push(" good_cp like '%"+good_cp+"%' ");
+		    }
+		    var filterquery = filter.join(" and ");
+		    if(filterquery){
+		        filterquery += " and ";
+            }
+			console.log(filterquery);
             pool.getConnection(function(err, conn) {
-                conn.query('select count(*) as total from good_info where good_company='+req.session.comp_id, function(err, ret){
+                conn.query('select count(*) as total from good_info where '+filterquery+' good_company='+req.session.comp_id, function(err, ret){
                     if(err){
                         return res.json({status: 500, err: err.message});
                     }
 
-                    conn.query('select good_info.* from good_info where good_company='+req.session.comp_id+' order by good_id desc limit '+start+','+limit, function(err, datas){
+                    conn.query('select good_info.* from good_info where '+filterquery+' good_company='+req.session.comp_id+' order by good_id desc limit '+start+','+limit, function(err, datas){
                         if(err){
                             return res.json({status: 500, err: err.message});
                         }
